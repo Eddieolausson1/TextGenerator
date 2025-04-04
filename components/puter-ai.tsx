5aca28e331feb83ad0a5a1cd1254c8cde73bba3c
Eddieolausson1 <94015092+Eddieolausson1@users.noreply.github.com>
2025-04-04 10:33:48 +0200

    Created puter-ai.tsx

---
+++ components/puter-ai.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const PuterAi: React.FC = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://js.puter.com/v2/";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+export default PuterAi;
